289502b700c2562c0802403cd8ce36213a5a5b11
Fernando Cate <fernandoecate@gmail.com>
2022-11-28 11:03:35 -0300

    [FIX] Arreglado error de deploy en DB

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
-const { REACT_APP_apiKey} = process.env
-const firebaseConfig ={
-  apiKey: REACT_APP_apiKey,
-authDomain: process.env.REACT_APP_authDomain,
-projectId: process.env.REACT_APP_projectId,
-storageBucket: process.env.REACT_APP_storageBucket,
-messagingSenderId: process.env.REACT_APP_messagingSenderId,
-appId: process.env.REACT_APP_appId,
-measurementId: process.env.REACT_APP_measurementId}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDir49gvJ2okJpA5FNzTU7leIcK8xV0d2U",
+  authDomain: "museum-397f4.firebaseapp.com",
+  projectId: "museum-397f4",
+  storageBucket: "museum-397f4.appspot.com",
+  messagingSenderId: "291703737397",
+  appId: "1:291703737397:web:6b5430c924e34ef0a2757c",
+  measurementId: "G-KV5PCWFHBM"
+};
 
 const app = initializeApp(firebaseConfig);
 
